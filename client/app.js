89198b714249eea325af322f417f5442887a7484
Akos Szokodi <szokodiakos@gmail.com>
2016-08-03 20:32:41 +0200

    Finished

--- client/app.js
+++ client/app.js
@@ -2,7 +2,17 @@
   var API_BASE = 'http://localhost:3000';
 
   var urls = {
-    ANIMALS: function() { return API_BASE + '/api/v1/animals' }
+    ANIMALS: function() {
+      return API_BASE + '/api/v1/animals';
+    },
+    ANIMAL: function(name) {
+      var api = API_BASE + '/api/v1/animal';
+
+      if (name) {
+        api = api + '/' + name;
+      }
+      return api;
+    }
   };
 
   angular
@@ -11,6 +21,9 @@
       var vm = this;
       vm.animalTypeFilter = '';
       vm.animalNames = '';
+      vm.newAnimalName = '';
+      vm.newAnimalType = '';
+      vm.animalNameToDelete = '';
 
       vm.getAnimals = function getAnimals() {
         $http.get(urls.ANIMALS(), {
@@ -18,19 +31,51 @@
             type: vm.animalTypeFilter,
           }
         })
-          .then(function(res) {
-            var animals = res.data;
-            var animalNames = animals.map(function(animal) {
-              return animal.name;
-            });
-
-            vm.animalNames = animalNames.join(', ');
-          })
-          .catch(function(res) {
-            console.log(res);
-            var errorMessage = res.data.message;
-            vm.animalNames = 'Error: ' + errorMessage;
+        .then(function(res) {
+          var animals = res.data.item;
+          var animalNames = animals.map(function(animal) {
+            return animal.name;
           });
+
+          vm.animalNames = animalNames.join(', ');
+        })
+        .catch(function(res) {
+          var errorMessage = res.data.message;
+          vm.animalNames = 'Error: ' + errorMessage;
+        });
+      }
+
+      vm.postAnimal = function postAnimal() {
+        $http.post(urls.ANIMAL(), {
+          animal: {
+            name: vm.newAnimalName,
+            type: vm.newAnimalType
+          }
+        })
+        .then(function(res) {
+          var message = res.data.item;
+          vm.newAnimalMessage = message;
+        })
+        .catch(function(res) {
+          var errorMessage = res.data.message;
+          vm.newAnimalMessage = 'Error: ' + errorMessage;
+        });
+      }
+
+      vm.deleteAnimal = function deleteAnimal() {
+        if (!vm.animalNameToDelete) {
+          vm.deletedAnimalMessage = 'Error: Name must be provided.';
+          return;
+        }
+        $http.delete(urls.ANIMAL(vm.animalNameToDelete))
+        .then(function(res) {
+          var message = res.data.item;
+          vm.deletedAnimalMessage = message;
+        })
+        .catch(function(res) {
+          var errorMessage = res.data.message;
+          vm.deletedAnimalMessage = 'Error: ' + errorMessage;
+        });
       }
     });
 })();
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Boom = require('boom');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
@@ -13,6 +14,8 @@ const animals = [{
   },
 ];
 
+app.use(bodyParser.json());
+
 app.use('/client', express.static('client'));
 
 app.get('/api/v1/animals', (req, res, next) => {
@@ -23,16 +26,48 @@ app.get('/api/v1/animals', (req, res, next) => {
     if (!filteredAnimals.length) {
       return next(Boom.badRequest(`No such animalType: ${animalType}.`));
     }
-    return res.json(filteredAnimals);
+    return res.json({ item: filteredAnimals });
   }
 
-  return res.json(animals);
+  return res.json({ item: animals });
 });
+
+app.post('/api/v1/animal', (req, res, next) => {
+  const newAnimal = req.body.animal;
+  if (!newAnimal.name) {
+    return next(Boom.badRequest(`Animal must have a name.`));
+  }
+
+  const isAnimalAlreadyExists = animals.find(animal => animal.name === newAnimal.name);
+  if (isAnimalAlreadyExists) {
+    return next(Boom.badRequest(`Animal with name ${newAnimal.name} already exists.`));
+  }
+
+  animals.push(newAnimal);
+  return res.json({ item: `Animal with name ${newAnimal.name} added!` });
+});
+
+app.delete('/api/v1/animal/:name', (req, res, next) => {
+  const animalName = req.params.name;
+
+  if (!animalName) {
+    return next(Boom.badRequest('Name must be provided.'));
+  }
+
+  const indexOfAnimal = animals.findIndex(animal => animal.name === animalName);
+  if (indexOfAnimal === -1) {
+    return next(Boom.badRequest(`Animal with name ${animalName} does not exist.`));
+  }
+
+  animals.splice(indexOfAnimal, 1);
+  return res.json({ item: `Animal with name ${animalName} deleted!`});
+})
 
 app.use((err, req, res, next) => {
   if (err.isBoom) {
     return res.status(err.output.statusCode).json(err.output.payload);
   }
+  console.log(err.stack);
   return res.status(500).json(err);
 })
 
